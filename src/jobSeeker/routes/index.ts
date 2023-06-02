9c94704a55b1b27617cb668f987fff8422e1737c
Ayush kumar <kumarayushkumar@protonmail.com>
2023-06-02 12:50:28 +0530

    jobseeker partial completed

--- src/jobSeeker/routes/index.ts
+++ src/jobSeeker/routes/index.ts
@@ -1,55 +1,51 @@
-import upload from '../../middleware/multer';
+
 import express from 'express'
+import { postVerify } from '../../middleware/post-verify';
 import {
   aboutMeHandler,
   addEducationHandler,
   updateEducationHandler,
   deleteEducationHandler,
-  // addWorkExperienceHandler,
-  // updateWorkExperienceHandler,
-  // deleteWorkExperienceHandler,
-  // addLanguageHandler,
   updateLanguageHandler,
   deleteLanguageHandler,
   addSkillHandler,
-  // getAppliedJobsHandler,
-  // applyJobHandler,
-  // saveJobHandler,
-  // getSavedJobsHandler,
-  // deleteSavedJobHandler,
+  getAppliedJobsHandler,
+  applyJobHandler,
+  saveJobHandler,
+  getSavedJobsHandler,
+  deleteSavedJobHandler,
 } from '../controllers/index';
 
 const router = express.Router();
 
 // Profile routes
-router.patch('/about-me', aboutMeHandler);
+router.patch('/about-me', postVerify, aboutMeHandler);
 
 // Education routes
-router.post('/educations', addEducationHandler);
-router.patch('/educations/:educationId', updateEducationHandler);
-router.delete('/educations/:educationId', deleteEducationHandler);
+router.post('/educations', postVerify, addEducationHandler);
+router.patch('/educations/:educationId', postVerify, updateEducationHandler);
+router.delete('/educations/:educationId', postVerify, deleteEducationHandler);
 
-// Experience routes
+// // Experience routes
 // router.post('/work-experiences', addWorkExperienceHandler);
 // router.patch('/work-experiences/:workExperienceId', updateWorkExperienceHandler);
 // router.delete('/work-Experience/:workExperienceId', deleteWorkExperienceHandler);
 
 // // Language routes
 // router.post('/language', addLanguageHandler);
-router.patch('/languages/:languageId', updateLanguageHandler);
-router.delete('/languages/:languageId', deleteLanguageHandler);
+router.patch('/languages/:languageId', postVerify, updateLanguageHandler);
+router.delete('/languages/:languageId', postVerify, deleteLanguageHandler);
 
 // Skill routes
-router.post('/skills', addSkillHandler);
+router.post('/skills', postVerify, addSkillHandler);
 
-// Job application routes
-// router.get('/jobs/apply', getAppliedJobsHandler);
-// router.post('/jobs/apply/:jobId', applyJobHandler);
+router.get('/jobs/apply', postVerify, getAppliedJobsHandler);
+router.post('/jobs/apply/:jobId', postVerify, applyJobHandler);
 
-// // Saved jobs routes
-// router.post('/jobs/save/:jobId', saveJobHandler);
-// router.get('/jobs/save', getSavedJobsHandler);
-// router.delete('/jobs/save/:jobId', deleteSavedJobHandler);
+// Saved jobs routes
+router.post('/jobs/save/:jobId', postVerify, saveJobHandler);
+router.get('/jobs/save', postVerify, getSavedJobsHandler);
+router.delete('/jobs/save/:jobId', postVerify, deleteSavedJobHandler);
 
 
 
